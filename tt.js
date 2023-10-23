5ef611064263daff0728399f9a3eefbb58989397
liukun <liukun0227@163.com>
2023-10-23 23:52:50 +0800

    docs: xixi

--- tt.js
+++ tt.js
@@ -3,7 +3,7 @@
  * @version: 1.0.0
  * @Author: liukun
  * @Date: 2023-10-12 11:39:19
- * @LastEditTime: 2023-10-23 22:06:01
+ * @LastEditTime: 2023-10-23 23:08:59
  * @LastEditors: liukun liukun0227@163.com
  */
 //   let a = 900;
@@ -12,7 +12,7 @@ console.info(b)
 let ttt = function (a) {
   a + 90
 }
-let t2t = 90
+let t2t2 = 90
 
 const cc = strind => {
   console.info(strind.length)
